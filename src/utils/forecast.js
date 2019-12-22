ec2c7e3930267845d60dd192c11c11b38f7042c3
TCT Admin <tctadmin@TCTs-MacBook-Pro.local>
2019-12-22 15:19:39 -0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/d7ed2f203ab84adee9437976c58c9939/' + latitude + ',' + longitude
+
+    request ({url, json: true }, (error, { body }) => {
+            if (error){
+                callback('Unable to connect to weather service!', undefined)
+            } else if (body.error) {
+                callback('Unable to find location', undefined)
+            } else {
+                callback(undefined, body.daily.data[0].summary  + ' It is currently ' + body.currently.temperature + ' out with a ' + body.currently.precipProbability + '% chance of rain.')
+
+            }
+
+    })
+}
+
+module.exports = forecast
